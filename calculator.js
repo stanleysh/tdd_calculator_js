236e2002b12906544cade24a8e1d64154b8fbf7b
Stanley Sham <stanley.sham94@gmail.com>
2019-08-29 14:08:35 -0400

    completed assignment

--- calculator.js
+++ calculator.js
@@ -1,10 +1,13 @@
-function add() {
+function add(a, b) {
+    return a + b
 }
 
-function subtract() {
+function subtract(a, b) {
+    return a - b
 }
 
-function sum() {
+function sum(array) {
+    return array.reduce((a, b) => a + b, 0)
 }
 
 module.exports = {add, subtract, sum}
